0c3db928f0d204fd589df6cdc23bf54b822e8307
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-12 19:19:48 -0400

    adding words

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import Popup from "./components/Popup";
 import Notification from "./components/Notification";
+import { useState } from "react";
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
   return (
     <>
       <Header />
