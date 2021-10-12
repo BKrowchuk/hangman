5a54b79b2667c062edc1082b00ad7398007a4df9
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-12 19:13:27 -0400

    added notification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import Popup from "./components/Popup";
+import Notification from "./components/Notification";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Word />
       </div>
       <Popup />
+      <Notification />
     </>
   );
 }
