4f2dcc8b240b2f400237f20351586830b1e5b1cd
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-12 19:05:38 -0400

    adding css and figure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Figure from "./components/Figure";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </>
   );
 }
