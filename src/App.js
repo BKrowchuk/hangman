3ccfa5b1931571f4a809846aad599abdebf91649
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-12 18:52:42 -0400

    adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Bens Hangman</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
