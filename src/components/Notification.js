63f55fa95d8538f3e8370881e31204c35ae2dca7
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-10-13 14:13:40 -0400

    figure and notification showing up now

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,6 @@
-const Notification = () => {
+const Notification = ({ showNotification }) => {
   return (
-    <div className="notification-container">
+    <div className={`notification-container ${showNotification ? "show" : ""}`}>
       <p>You have already entered this letter</p>
     </div>
   );
